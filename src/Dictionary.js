836c0cce2eb60bde6609840c3ec97f78e10306d6
dianainberlin <145753245+dianainberlin@users.noreply.github.com>
2024-03-10 17:17:59 +0100

    updated the search bar function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
     alert("Searching 👾");
   }
 
+  function handleKeywordChange(event) {
+    console.log(event);
+  }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true}></input>
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+        ></input>
       </form>
     </div>
   );
