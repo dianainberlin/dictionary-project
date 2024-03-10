8a54aad4920776a2e7ddb36f54e14cacdc74d612
dianainberlin <145753245+dianainberlin@users.noreply.github.com>
2024-03-10 18:05:47 +0100

    installed axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiKey = "fdbbb67f1d9b8ba71b3b07f3d6t4a6od";
+    let word = "book";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
